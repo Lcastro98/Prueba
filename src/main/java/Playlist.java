1786c6fd15ff65cc7592419357fec46ef181350c
wianpedi <wianpedi@gmail.com>
2022-02-14 05:31:56 +0100

    SE CREARON LOS METODOS

--- src/main/java/Playlist.java
+++ src/main/java/Playlist.java
@@ -2,7 +2,22 @@ public class Playlist extends Song {
     public String pause() {
         return "Se Pauso La Cancion";
     }
-    public String Play () {
+
+    public String play() {
         return "Se Esta Reproduciendo";
+    }
+
+    public String forward() {
+        return "Se Adelanto la cancion 10s";
+    }
+
+    public String backlog() {
+        return "Se Atrazo la cancion 10s";
+    }
+    public String filterGender(){
+        return "filtrar genero";
+    }
+    public String filterYear(){
+        return"Filtrar Año";
     }
 }
